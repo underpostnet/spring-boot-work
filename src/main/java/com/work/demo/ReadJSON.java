1e037ff97db34ae783be0a5bbfe190bc2a9962df
fcoverdugo <fcoverdugoa@underpost.net>
2022-11-23 11:13:44 -0300

    update

--- src/main/java/com/work/demo/ReadJSON.java
+++ src/main/java/com/work/demo/ReadJSON.java
@@ -3,6 +3,7 @@ package com.work.demo;
 import java.io.IOException;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.work.demo.modules._files;
@@ -10,8 +11,8 @@ import com.work.demo.modules._paths;
 
 @RestController
 public class ReadJSON {
-    @RequestMapping("/json")
-    public String hello() throws IOException {
+    @RequestMapping(value = { "json", "json-test" }, method = RequestMethod.GET)
+    public String a() throws IOException {
         _paths confPath = new _paths("C:/dd/spring-boot/main/src/main/java/com/work/demo/");
         return _files.getRawContent(confPath.getPathFromRelative("./modules/test.json"));
     }
